7fd8b9210716d1c99a128cb0fd0b7ebe00b4d782
Alexander <temnoved@yandex.ru>
2019-05-15 14:21:35 +0300

    Add files via upload

--- start_centos.sh
+++ start_centos.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 if [[ "${LOGNAME}" != "root" ]]; then
-  echo "Sorry. Only root can start the script." >&2
-  exit 1
-fi
+	  echo "Sorry. Only root can start the script." >&2
+		  exit 1
+		fi
 
-yum update -yy \
-	&& yum upgrade -yy \
-	&& yum -yy install wget vim htop net-tools gpm bind-utils
+		yum update -yy \
+				&& yum upgrade -yy \
+					&& yum -yy install wget vim htop net-tools gpm bind-utils tree
 
-yum install -yy https://centos7.iuscommunity.org/ius-release.rpm \
-        && yum install -yy python36u python36u-devel python36u-pip
+		yum install -yy https://centos7.iuscommunity.org/ius-release.rpm \
+			        && yum install -yy python36u python36u-devel python36u-pip
 
-if [[ "$?" -eq 0 ]]; then
-  reboot
-fi
-exit 0
+		if [[ "$?" -eq 0 ]]; then
+			  reboot
+			fi
+			exit 0
